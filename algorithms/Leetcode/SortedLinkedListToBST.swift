0117409470fa8565913891baa8eaf8b09b2f3246
paulpires <paulpires101@gmail.com>
2022-02-01 18:33:50 +0000

    Convert Sorted List to Binary Search Tree

--- algorithms/Leetcode/SortedLinkedListToBST.swift
+++ algorithms/Leetcode/SortedLinkedListToBST.swift
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree
 class SortedLinkedListToBST
 {
-    // MARK: Solution 1
+    // MARK: - Solution 1
     // Could be more efficient
     func solution1(_ head: ListNode?) -> TreeNode?
     {
@@ -32,11 +32,30 @@ class SortedLinkedListToBST
         return node
     }
     
-    // MARK: Solution 2
-    private func solution2(_ head: ListNode?) -> TreeNode?
+    // MARK: - Solution 2
+    func solution2(_ head: ListNode?) -> TreeNode?
     {
-        // todo:
-        // using 2 pointers searching ahead
-        return nil
+        return head == nil ? nil : bst(head: head, tail: nil)
+    }
+    
+    private func bst(head: ListNode?, tail: ListNode?) -> TreeNode?
+    {
+        var slow = head
+        var fast = head
+        if head?.val == tail?.val
+        {
+            return nil
+            // nil because it's already in the tree!
+            // (it's the root of the current sub-tree)
+        }
+        while fast?.val != tail?.val && fast?.next?.val != tail?.val
+        {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        let node = TreeNode(slow!.val)
+        node.left = bst(head: head, tail: slow)
+        node.right = bst(head: slow!.next, tail: tail)
+        return node
     }
 }
--- algorithmsTests/SortedLinkedListToBSTTests.swift
+++ algorithmsTests/SortedLinkedListToBSTTests.swift
@@ -1,20 +1,53 @@
-//
-//  algorithmsTests.swift
-//  algorithmsTests
-//
-//  Created by Paul Pires on 29/01/2022.
-//
-
 import XCTest
 @testable import algorithms
 
+/**
+ E.g.:
+ Input:
+ ------------------------------------------------------------
+   [-10 -> -3 -> 0 -> 5 -> 9 -> 20 -> 21 -> 22 -> 23 -> 24]
+ ------------------------------------------------------------
+ Output:
+                            20
+                             |
+                |-------------------------|
+                0                        23
+          |-----------|            |-----------|
+         -3           9            22          24
+        |---x       |---x        |----x      |----x
+      -10  nil      5  nil      21   nil    nil  nil
+ 
+ Therefore, breadth first search output:
+ |
+ |-> [20, 0, 23, -3, 9, 22, 24, -10, nil, 5, nil, 21]
+*/
+
 class SortedLinkedListToBSTTests: XCTestCase
 {
-    func testA()
+    
+    // MARK: - Solution 1
+    func testSolution1()
     {
         let input = ListNode(-10, .init(-3, .init(0, .init(5, .init(9, nil)))))
         let expectedOut = [0, -3, 9, -10, nil, 5]
         let out = SortedLinkedListToBST().solution1(input)
+        XCTAssertEqual(out!.breadthFirstDescription(), expectedOut)
+    }
+    
+    // MARK: - Solution 2
+    func testSolution2_A()
+    {
+        let input = ListNode(-10, .init(-3, .init(0, .init(5, .init(9, nil)))))
+        let expectedOut = [0, -3, 9, -10, nil, 5]
+        let out = SortedLinkedListToBST().solution2(input)
+        XCTAssertEqual(out!.breadthFirstDescription(), expectedOut)
+    }
+
+    func testSolution2_B()
+    {
+        let input = ListNode(-10, ListNode(-3, ListNode(0, ListNode(5, ListNode(9, ListNode(20, ListNode(21, ListNode(22, ListNode(23, ListNode(24, nil))))))))))
+        let out = SortedLinkedListToBST().solution2(input)
+        let expectedOut = [20, 0, 23, -3, 9, 22, 24, -10, nil, 5, nil, 21]
         XCTAssertEqual(out!.breadthFirstDescription(), expectedOut)
     }
 }
