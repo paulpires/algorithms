aed93541c27b73e3160cd998af2d76024f3ee783
paulpires <paulpires101@gmail.com>
2022-02-13 13:05:48 +0000

    max subarr alternative

--- Sources/algorithms/Leetcode/MaximumSubArray.swift
+++ Sources/algorithms/Leetcode/MaximumSubArray.swift
@@ -13,4 +13,18 @@ class MaximumSubArray
         }
         return best
     }
+    
+    // MARK: - alternative
+
+    func maxSubArray2(_ nums: [Int]) -> Int
+    {
+        var best = nums[0], current = nums[0]
+        guard nums.count > 1 else { return best }
+        for n in nums[1..<nums.count]
+        {
+            current = max(current+n, n)
+            best = max(best, current)
+        }
+        return best
+    }
 }
--- Tests/algorithmsTests/MaximumSubArrayTests.swift
+++ Tests/algorithmsTests/MaximumSubArrayTests.swift
@@ -18,10 +18,36 @@ class MaximumSubArrayTests: XCTestCase
     func testC()
     {
         let input = [5,4,-1,7,8]
-        let out = MaximumSubArray().maxSubArray(input)
+        let out = MaximumSubArray().maxSubArray2(input)
         XCTAssertEqual(out, 23)
     }
     func testD()
+    {
+        let input = [-1]
+        let out = MaximumSubArray().maxSubArray2(input)
+        XCTAssertEqual(out, -1)
+    }
+    
+    // MARK: - alternative
+    func testE()
+    {
+        let input = [-2,1,-3,4,-1,2,1,-5,4]
+        let out = MaximumSubArray().maxSubArray2(input)
+        XCTAssertEqual(out, 6) // [4,-1,2,1]
+    }
+    func testF()
+    {
+        let input = [1]
+        let out = MaximumSubArray().maxSubArray2(input)
+        XCTAssertEqual(out, 1)
+    }
+    func testG()
+    {
+        let input = [5,4,-1,7,8]
+        let out = MaximumSubArray().maxSubArray(input)
+        XCTAssertEqual(out, 23)
+    }
+    func testH()
     {
         let input = [-1]
         let out = MaximumSubArray().maxSubArray(input)
